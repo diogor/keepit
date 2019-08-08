c2dfba8e9e386896765aa1ccd4444e6ebc27ef6b
Diogo Lima <diogo@dr6.com.br>
2019-08-08 16:28:14 -0300

    cors

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from environs import Env
 from fastapi import FastAPI
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 from starlette.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 from entities import ThingModel, ThingDocument
 
 app = FastAPI()
@@ -13,6 +14,19 @@ env = Env()
 env.read_env()
 database = env.str("DATABASE_URL")
 connect(host=database)
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def create_thing(thing: ThingModel) -> ThingModel:
